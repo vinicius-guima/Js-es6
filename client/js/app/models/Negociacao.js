54f819f997cd76dbcf2f5c831a5a168d09ff6103
vinicius-guima <vinicius_guima.social@hotmail.com>
2020-04-28 01:37:49 -0300

    and of chapter 2

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -2,17 +2,18 @@ class Negociacao {                           //classe
 
     constructor(data, quantidade, valor) {   //constutor
 
-        this._data = data;                    //atributos 
-        this._quantidade = quantidade;          //(_atr) = convenção de private 
+        this._data = new Date(data.getTime());                    //atributos 
+        this._quantidade = quantidade;        //(_atr) = convenção de private 
         this._valor = valor;
-                                             
+        Object.freeze(this);                  //não altera o obj depois da criação                       
     }
 
-    getVolume() { return this.quantidade * this.valor;}
+    //get atributo permite acessar como atributo mas o js usa o metodo get como getVolume(){}
+    get volume() { return this.quantidade * this.valor;}
 
-    getData(){ return this._data;}
+    get data(){ return new Date(this._data.getTime());}
 
-    getQuantidade(){ return this._quantidade;}
+    get quantidade(){ return this._quantidade;}
 
-    getValor(){ return this._valor;}
+    get valor(){ return this._valor;}
 }
