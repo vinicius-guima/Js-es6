bb77a5189ea64a97c8dbcaf62fe0461756b59286
vinicius-guima <vinicius_guima.social@hotmail.com>
2020-04-30 09:54:37 -0300

    add a total in table

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -4,7 +4,7 @@ class NegociacoesView {
         this._elemento = elemento;
     }
 
-    _template() {
+    _template(model) {
         return `<table class="table table-hover table-bordered">
                     <thead>
                         <tr>
@@ -16,16 +16,26 @@ class NegociacoesView {
                     </thead>
                 
                     <tbody>
+                    ${model.negociacoes.map(n => 
+                        `<tr>
+                            <td>${DataHelper.dataParaTexto(n.data)}</td>
+                            <td>${n.quantidade}</td>
+                            <td>${n.valor}</td>
+                            <td>${n.volume}</td>
+                        </tr> `
+                     ).join('')}
                     </tbody>
                 
                     <tfoot>
+                        <td colspan="3"></td>
+                        <td>${model.negociacoes.reduce((total, n) => total + n.volume,0.0)}</td>
                     </tfoot>
                  </table>
             `;
     }
 
-    update() {
-        this._elemento.innerHTML = this._template();
+    update(model) {
+        this._elemento.innerHTML = this._template(model);
     }
 
 }
