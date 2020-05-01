d11b86ea6a87ccbe4bcfaad4ddf8136f7a17f54f
vinicius-guima <vinicius_guima.social@hotmail.com>
2020-04-30 22:33:23 -0300

    patterns observer

--- client/js/app/controllers/NegociaocaoController.js
+++ client/js/app/controllers/NegociaocaoController.js
@@ -6,15 +6,16 @@ class NegociacaoController {
         this._inputData = $('#data');                  // se asemelha a sintaxe do jquery 
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+
+        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
-        
+
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
-        
+
     }
 
     adiciona(event) {
@@ -22,6 +23,13 @@ class NegociacaoController {
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
         this._mensagem.texto = 'Negociação adicionada com sucesso';
+        this._mensagemView.update(this._mensagem);
+        this._limpaFormulario();
+    }
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
         this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
